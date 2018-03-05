6546ef778ccb1e0b2b321beb87bb677c706a61ee
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2018-03-05 18:42:39 +0400

    Added files component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FilesComponent } from './files/files.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilesComponent
   ],
   imports: [
     BrowserModule
